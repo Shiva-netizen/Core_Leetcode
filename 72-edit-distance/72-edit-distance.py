f7d950537e71eda523d61019a5b086480cd6f57f
SHIVASHISH PRAJAPATI <74098683+Shiva-netizen@users.noreply.github.com>
2022-09-27 10:23:29 +0530

    Time: 496 ms (10.76%), Space: 13.9 MB (98.82%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def minDistance(self, word1, word2):
+        h, w = len(word1)+1, len(word2)+1
+        pre = [i for i in range(w)]
+        for i in range(1, h):
+            cur = [i for _ in range(w)]
+            for j in range(1, w):
+                cur[j] = min(pre[j-1]+(word1[i-1] != word2[j-1]), pre[j]+1, cur[j-1]+1)
+            pre = cur
+        return pre[-1]
